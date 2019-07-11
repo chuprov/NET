b7364c03c8ea139e99e9c682d20bd7102992cf2a
ChuprovNV <nik.adm@gmail.com>
2019-07-11 17:50:07 +0300

    11

--- Patterns/FactoryMethod.cs
+++ Patterns/FactoryMethod.cs
@@ -1,7 +1,26 @@
 ﻿namespace Patterns
 {
-    public class FactoryMethod
+    abstract class Product
+    { }
+
+    class ConcreteProductA : Product
+    { }
+
+    class ConcreteProductB : Product
+    { }
+
+    abstract class Creator
     {
-        
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductA(); }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductB(); }
     }
 }
