6f879e94b6a2767311207b07fb1891f9f2bebfe0
ChuprovNV <nik.adm@gmail.com>
2019-07-12 17:00:01 +0300

    22

---
+++ Patterns/AbstractFactory.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patterns
+{
+    //предоставляет интерфейс для создания семейств взаимосвязанных объектов с определенными интерфейсами 
+    //без указания конкретных типов данных объектов.
+    
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+
+    abstract class AbstractProductA
+    { }
+
+    abstract class AbstractProductB
+    { }
+
+    class ProductA1 : AbstractProductA
+    { }
+
+    class ProductB1 : AbstractProductB
+    { }
+
+    class ProductA2 : AbstractProductA
+    { }
+
+    class ProductB2 : AbstractProductB
+    { }
+
+    class Client
+    {
+        private AbstractProductA abstractProductA;
+        private AbstractProductB abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            abstractProductB = factory.CreateProductB();
+            abstractProductA = factory.CreateProductA();
+        }
+        public void Run()
+        { }
+    }
+
+
+
+
+
+
+
+
+    //абстрактный класс - оружие
+    abstract class Weapon
+    {
+        public abstract void Hit();
+    }
+    // абстрактный класс движение
+    abstract class Movement
+    {
+        public abstract void Move();
+    }
+
+    // класс арбалет
+    class Arbalet : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Стреляем из арбалета");
+        }
+    }
+    // класс меч
+    class Sword : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Бьем мечом");
+        }
+    }
+    // движение полета
+    class FlyMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Летим");
+        }
+    }
+    // движение - бег
+    class RunMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Бежим");
+        }
+    }
+    // класс абстрактной фабрики
+    abstract class HeroFactory
+    {
+        public abstract Movement CreateMovement();
+        public abstract Weapon CreateWeapon();
+    }
+    // Фабрика создания летящего героя с арбалетом
+    class ElfFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new FlyMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Arbalet();
+        }
+    }
+    // Фабрика создания бегущего героя с мечом
+    class VoinFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new RunMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Sword();
+        }
+    }
+    // клиент - сам супергерой
+    class Hero
+    {
+        private Weapon weapon;
+        private Movement movement;
+        public Hero(HeroFactory factory)
+        {
+            weapon = factory.CreateWeapon();
+            movement = factory.CreateMovement();
+        }
+        public void Run()
+        {
+            movement.Move();
+        }
+        public void Hit()
+        {
+            weapon.Hit();
+        }
+    }
+
+
+
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -9,6 +9,28 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
+
+            //Factory Method
+            Developer dev = new PanelDeveloper("ООО КирпичСтрой");
+            House house2 = dev.Create();
+
+            dev = new WoodDeveloper("Частный застройщик");
+            House house = dev.Create();
+
+            Console.ReadLine();
+
+
+
+            //Abstract Factory
+            Hero elf = new Hero(new ElfFactory());
+            elf.Hit();
+            elf.Run();
+
+            Hero voin = new Hero(new VoinFactory());
+            voin.Hit();
+            voin.Run();
+
+            Console.ReadLine();
         }
     }
 }
